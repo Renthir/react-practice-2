438601c5b7cf186ce9160a7242eeb9ce18939fe2
Seth Reid <reid.seth.96@gmail.com>
2023-06-16 23:19:19 -0600

    more component work

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import ExpenseItem from "./components/ExpenseItem";
-import AnotherComponent from "./components/another-component";
 
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
       <p>This is also visible!</p>
-      <AnotherComponent></AnotherComponent>
       <ExpenseItem></ExpenseItem>
     </div>
   );
